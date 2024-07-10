235d61a71a2903832b3862930d5e422f51366e9c
Karan-Tatva24 <karan.sarvaiya@etatvasoft.com>
2024-07-10 15:16:24 +0530

    add metadata to the all pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,23 @@
 import '@/app/ui/global.css'
 import { inter } from "@/app/ui/fonts"
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: { template: "%s | Acme Dashboard", default: 'Acme Dashboard' },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  authors: [{ name: "Tatvasoft" }, { name: "Karan Sarvaiya" }],
+  keywords: ['Next.js', 'Acme', "Javascript", "React"],
+  referrer: 'origin-when-cross-origin',
+  creator: "Karan Sarvaiya",
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  formatDetection: {
+    email: true,
+    address: false,
+    telephone: true,
+  },
+  generator: 'Next.js',
+  applicationName: 'Acme Dashboard',
+};
 
 export default function RootLayout({
   children,
