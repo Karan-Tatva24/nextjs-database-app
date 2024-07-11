d8d74058b18e7d41c9af099688222c87b20ed4e3
Karan-Tatva24 <karan.sarvaiya@etatvasoft.com>
2024-07-11 11:13:09 +0530

    Add customer table to customer page with search functionality

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,28 @@
+import { fetchCustomers, fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
     title: "customer"
 }
 
-export default function Page() {
-    return <p>Customer Page.</p>
+export default async function Page({ searchParams }: {
+    searchParams: {
+        query?: string,
+        page?: string
+    }
+}) {
+    const query = searchParams?.query || "";
+    let customerDetails = [];
+    if (query) {
+        customerDetails = await fetchFilteredCustomers(query);
+    } else {
+        const fetchedCustomers = await fetchCustomers();
+        for (let i = 0; i < fetchedCustomers.length; i++) {
+            const customer = await fetchFilteredCustomers(fetchedCustomers[i].name);
+            customerDetails.push(customer);
+        }
+    }
+
+    return <CustomersTable customers={customerDetails.flat()} />
 }
